a9d9ada12747e087c4aa772a84b17eb4e5b23edd
Aleksandr Mikheev <qqalexqq@yandex.ru>
2014-12-26 01:24:39 +0300

    Added database-related configuration and management

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 """Monkeys app exercise."""
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(os.environ.get('APP_SETTINGS', 'config.TestingConfig'))
+app.config.from_object(
+    os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig')
+)
+db = SQLAlchemy(app)
 
 # We run app only when it's really needed
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+"""Main configuration file."""
+import os
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_ECHO = False
 
 
 class ProductionConfig(Config):
@@ -15,7 +21,14 @@ class StagingConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'postgresql://localhost/monkeys_dev'
+    )
 
 
 class TestingConfig(Config):
+    DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'TEST_DATABASE_URL', 'postgresql://localhost/monkeys_test'
+    )
