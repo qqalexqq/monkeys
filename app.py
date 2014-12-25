3150135dd2e87017aaa22c68a0d8911c6eeeb8cf
Aleksandr Mikheev <qqalexqq@yandex.ru>
2014-12-26 01:09:21 +0300

    Initial configuration added.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 """Monkeys app exercise."""
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS', 'config.TestingConfig'))
 
 # We run app only when it's really needed
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,21 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
